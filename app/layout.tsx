6ffccf07bbd3ae9a1269f0b614b11d5a11b28f96
CodingHxx <sanraj2200bgf@gmail.com>
2024-10-29 21:26:15 +0530

    adasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <body ref={scrollRef} className={`font-[Gilroy] antialiased scrollbar-thin relative bg-black`}>
                 <link rel="stylesheet" href="https://fonts-dsd1.vercel.app/globals.css" />
+                <link rel="icon" href="/favicon.ico" sizes="any" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
                 <Cursor />
                 <ThemeProvider attribute="class" defaultTheme="dark">
